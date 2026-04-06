edd1ff80f0fa06916839746ace002d7ea2fe125c
kokomi-n <bonochan0625@gmail.com>
2026-04-07 06:02:51 +0900

    add to homework 9

---
+++ Homework 9/myrealself.js
@@ -0,0 +1,39 @@
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  background(185, 237, 251);
+noStroke();
+fill(112, 59, 11);
+rect(100, 160, 200, 220);
+circle(200, 170, 200);
+
+fill(255, 239, 203);
+ circle(200,200,200);
+ellipse(100,200,40,50);
+ellipse(300,200,40,50);
+
+fill(112, 59, 11);
+rect(128, 100, 144, 80);
+
+
+fill(0);
+circle(150,200,20);
+circle(250,200,20);
+
+fill(255, 0, 0);
+triangle(170, 250, 230, 250, 200, 280);
+
+fill(255, 230, 138);
+triangle(190, 230, 210, 230, 200, 200);
+
+stroke(0);
+line(130, 170, 170, 170);
+line(230, 170, 270, 170);
+
+stroke(255);
+point(150, 200);
+point(250, 200)
+
+}
